a0ee4785863bd8b787a17c99618582bcaef85239
spaceadh <spacesealadhesives13@gmail.com>
2025-09-06 13:41:03 +0300

    Clean/clearing unnecessary navbars

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,5 +1,4 @@
 import { createBrowserRouter } from 'react-router-dom';
-import { ModuleLayout } from './layout/ModuleLayout';
 import ProtectedRoute from './layout/ProtectedRoute';
 import LoginPage from '@/modules/auth/pages/LoginPage';
 import RegisterPage from '@/modules/auth/pages/RegisterPage';
